34ceb4a1c724a2424a34382160b464863a7e06b2
muhamadghibran <ripkaka91@gmail.com>
2025-02-26 21:31:43 +0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: Center(child: Text("Hello World")),
+        appBar: AppBar(title: Text("My App")),
+      ),
+    );
+  }
+}
